a2f82506874cceae1f156266763211acd1f315f0
xiang3 <18500400275@163.com>
2023-05-16 12:56:03 +0800

    add print which any one of matched

---
+++ example/awk/print_name_age_with_specific_name.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# reset
+trap 'rm example.csv' EXIT
+#given
+echo 'name, sex, age
+杜建, 女, 89
+杜世, 女, 58
+陆刚, 女, 4
+薛利, 男, 68
+冯宁, 男, 44' > example.csv
+echo "The example.csv detail:"
+echo ""
+cat example.csv
+echo ""
+
+# 查出指定列数据并指定特定数据:
+echo "查出名字中包含杜字的name, age俩列数据:"
+echo "running..."
+echo ""
+# when todo
+
+actual="TODO"
+
+
+# then
+echo "执行结果为："
+result=$(diff -i <( echo '杜建, 89
+杜世, 58' ) <( echo "$actual" ))
+
+if [ -n "$result" ]; then
+    echo "$result"
+else
+    echo "Match Success! this result:"
+    echo "$actual"
+fi
+
--- example/resource/awk.sh
+++ example/resource/awk.sh
@@ -1,3 +1,7 @@
 echo "按列分割文件并进行打印: awk '{print $1, $3}' example.csv"
 awk '{print $1, $3}' example.csv
 echo "\n"
+
+echo "查出名字中包含杜字的name, age俩列数据: awk '/杜/ {print $1, $3}' example.csv"
+awk '/杜/ {print $1, $3}' example.csv
+echo "\n"
